2cafe8c5084731c32c73ba4eec133ab83697e0b3
Eric Marrone <emarrone17@gmail.com>
2017-10-31 18:20:09 -0400

    Complete exercise 8

--- exercise8.rb
+++ exercise8.rb
@@ -26,20 +26,25 @@ distance3 = gets.to_f
 time?("person 3", distance3)
 mins3 = gets.to_f
 
-# speed1 = speed(distance1, mins1)
-# speed2 = speed(distance2, mins2)
-# speed3 = speed(distance3, mins3)
+speed1 = speed(distance1, mins1)
+speed2 = speed(distance2, mins2)
+speed3 = speed(distance3, mins3)
 
-# p speed(100, 2)
+if speed3 > speed2 && speed3 > speed1
+  puts "Person 3 was the fastest at #{speed3} m/s"
+elsif speed2 > speed3 && speed2 > speed1
+  puts "Person 2 was the fastest at #{speed2} m/s"
+elsif speed1 > speed3 && speed1 > speed2
+  puts "Person 1 was the fastest at #{speed1} m/s"
+elsif speed1 == speed2 && speed2 == speed3
+  puts "Everyone tied at #{speed1} m/s"
+else
+  puts "Well done everyone!"
+end
 
-# puts get_user_input("distance1")
 
 
-
-
-
-
-# --------------
+# --------------Original code
 
 # puts "How far did person 1 run (in metres)?"
 # distance1 = gets.to_f
